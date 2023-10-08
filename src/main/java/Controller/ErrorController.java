f9049b8e0364be391404b9a26b274cbb06a6d3d1
fadliyandra <fadliyandra27.fy@gmail.com>
2023-10-09 02:02:15 +0700

    add sucurtiy and exception

---
+++ src/main/java/Controller/ErrorController.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import jakarta.validation.ConstraintViolationException;
+import model.WebResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorController {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<WebResponse<String>>
+}
